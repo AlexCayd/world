2971fba1cbdebd2bcec3a1bde7a9d2466e2e80bb
Isaiah-Work <isaiah.rivasc@gmail.com>
2024-11-19 19:45:34 -0600

    Termine con el segundo query

--- app/queries.py
+++ app/queries.py
@@ -1,4 +1,5 @@
 from models import City, Country, CountryLanguage, SessionLocal
+from sqlalchemy.sql import func, over
 
 # Aqui definimos funciones para interactuar con la base de datos
 
@@ -20,3 +21,40 @@ def consulta_1():
          ]# Devuelve una lista como diccionarios
      finally:
          session.close()
+
+def consulta_2():
+    session = SessionLocal()
+    try:
+        rankedCities = session.query(
+        City.name,
+        City.population,
+        Country.name.label('country'),
+        Country.continent,
+        func.row_number().over(
+        order_by=City.population.desc(),
+        partition_by=Country.continent
+        ).label('rank')
+        ).join(Country, City.countryCode == Country.code).subquery()
+
+        topCities = session.query(
+        rankedCities.c.continent,
+        rankedCities.c.name,
+        rankedCities.c.population,
+        rankedCities.c.country
+        ).filter(
+            rankedCities.c.rank <= 5
+        ).order_by(
+            rankedCities.c.continent,
+            rankedCities.c.rank
+        ).all()
+        return [
+            {
+                "continent": topCity[0],
+                "city": topCity[1],
+                "population": topCity[2],
+                "country": topCity[3]
+            }
+            for topCity in topCities
+        ]
+    finally:
+        session.close()
